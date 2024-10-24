776df4f4c70b0c1f12ac971f3ccc280ff4363e63
abrahamx02 <georssj@gmail.com>
2024-10-24 12:25:48 -0500

    completo

--- Ejercicio08/src/ejercicio08/Ejercicio08.java
+++ Ejercicio08/src/ejercicio08/Ejercicio08.java
@@ -15,6 +15,24 @@ public class Ejercicio08 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    int valor1 = 10;
+    int valor2 = 9;
+    int valor3 = 20; 
+    int valor4 = 500;
+    int valor5 = 2;
+    int valor6 = 3;
+    int valor7 = 200; 
+    int valor8 = 50;
+    int valor9 = 51;
+    double resultado = (((valor1*valor2)+valor3-(valor4/valor5)+(Math.pow(valor6, valor5))));
+    double resultado2 = (valor8-valor8+valor9);
+    boolean a = resultado >= valor7; 
+    boolean b = valor8 >= resultado2;
+    boolean c = a || b; 
+    System.out.println(c);
+    //utilizamos variables y elementos de java
+    
+    
     }
     
 }
---
+++ Ejercicio10/src/ejercicio10/Ejercicio10.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio10;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int valor1 = 25;
+    int valor2 = 10;
+    int valor3 = 100; 
+    int valor4 = 5;
+    int valor5 = 2;
+    double resultado = ((Math.sqrt(valor1))*valor2);
+    double resultado2 = (valor2/valor4);
+    boolean a = resultado >= valor3;
+    boolean b = a && true; 
+    boolean c = b || false; 
+    boolean d = c || (resultado2>=valor5);
+    System.out.println(d);
+    //se acabo
+    //utuilizamos variables enteras doubles y booleanos 
+    }
+    
+}
